c44abef5df5cebf3786b47a40670eccface3dd00
Jakub Wyciślik <jakuwyc150@student.polsl.pl>
2020-05-27 14:19:15 +0200

    Update EhurtowniaContext.cs

--- E-hurtownia/E-hurtownia/Models/EhurtowniaContext.cs
+++ E-hurtownia/E-hurtownia/Models/EhurtowniaContext.cs
@@ -36,7 +36,6 @@ namespace E_hurtownia.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=E-hurtownia;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
