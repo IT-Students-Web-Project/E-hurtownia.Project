31c75b85b01bd32c85c9bbf379164634c8b7356a
Jakub Skowron <jskowron12@gmail.com>
2020-06-04 14:46:31 +0200

    ConfigUtils.cs

--- E-hurtownia/E-hurtownia/Utils/ConfigUtils.cs
+++ E-hurtownia/E-hurtownia/Utils/ConfigUtils.cs
@@ -6,14 +6,25 @@ namespace E_hurtownia.Utils
     public class ConfigUtils
     {
         public static string CONFIG_INI_PATH = "config.ini";
+        public static string VERSION_TXT_PATH = "version.txt";
 
         public static string GetDbConnectionString()
         {
-            Ini config = new Ini(CONFIG_INI_PATH);
-            if (config.ContainsKey("connectionString"))
-                return config["connectionString"];
+            return GetValueFromFile(ConfigParams.CONNECTION_STRING, CONFIG_INI_PATH);
+        }
+
+        public static string GetVersion()
+        {
+            return GetValueFromFile(ConfigParams.APP_VERSION, VERSION_TXT_PATH);
+        }
+
+        public static string GetValueFromFile(string key, string filePath)
+        {
+            Ini config = new Ini(filePath);
+            if (config.ContainsKey(key))
+                return config[key];
             else
-                throw new Exception("Key: 'connectionString' not found in file:" + CONFIG_INI_PATH);
+                throw new Exception("Key: " + key + "not found in file:" + filePath);
         }
     }
 }
