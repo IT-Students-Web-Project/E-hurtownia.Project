48639d033cc564b7dcff4f1e798977d62ebe434e
Jakub Skowron <jskowron12@gmail.com>
2020-06-04 14:46:48 +0200

    ConfigParams.cs

---
+++ E-hurtownia/E-hurtownia/Utils/ConfigParams.cs
@@ -0,0 +1,8 @@
+﻿namespace E_hurtownia.Utils
+{
+    static internal class ConfigParams
+    {
+        internal static string CONNECTION_STRING = "connectionString";
+        internal static string APP_VERSION = "appVersion";
+    }
+}
