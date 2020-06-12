87dc5fcb6e72ced977e40f9cdc3a905e0f6e1963
Jakub Wyciślik <jakuwyc150@student.polsl.pl>
2020-06-12 19:02:20 +0200

    Added shopping cart

---
+++ E-hurtownia/E-hurtownia/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using E_hurtownia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_hurtownia.Controllers {
+    public class CartController : Controller {
+        private EhurtowniaContext databaseContext = new EhurtowniaContext();
+
+        public IActionResult Index() {
+            ViewBag.COOKIE_LOGGED_USERNAME = Request.Cookies["COOKIE_LOGGED_USERNAME"];
+            ViewBag.Products = databaseContext.Products.ToList();
+            ViewBag.Units = databaseContext.Units.ToList();
+            string cartJSON = Request.Cookies["COOKIE_CART_CONTENT"];
+
+            List<CartElement> cartList = JsonSerializer.Deserialize<List<CartElement>>(cartJSON);
+
+            return View(cartList);
+        }
+    }
+}
