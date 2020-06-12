ed8c3e1a958afa7af10a47ecb414942ffb3808ee
Jakub Wyciślik <jakuwyc150@student.polsl.pl>
2020-06-12 14:25:19 +0200

    Added individual product description page

--- E-hurtownia/E-hurtownia/Controllers/CustomerController.cs
+++ E-hurtownia/E-hurtownia/Controllers/CustomerController.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_hurtownia.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_hurtownia.Controllers {
     public class CustomerController : Controller {
-        public IActionResult Index() {
+        private EhurtowniaContext databaseContext = new EhurtowniaContext();
+
+        public IActionResult Product(int id) {
+            ViewBag.COOKIE_LOGGED_USERNAME = Request.Cookies["COOKIE_LOGGED_USERNAME"];
+            ViewBag.Product = databaseContext.Products.Where(product => product.IdProduct == id).Single();
+            ViewBag.Stocks = databaseContext.Stocks.ToList();
+            ViewBag.Units = databaseContext.Units.ToList();
+
             return View();
         }
     }
