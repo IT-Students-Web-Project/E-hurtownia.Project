8fb13d5c52ef347b7c45df564c1b257a6778c761
Jakub Skowron <jskowron12@gmail.com>
2020-06-21 18:38:10 +0200

    Correction with OrderItems

---
+++ E-hurtownia/E-hurtownia/Models/CartElement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_hurtownia.Models {
+    public class CartElement {
+        public int ProductID { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
